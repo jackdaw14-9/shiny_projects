588c79ad6b74a979f1cd066547f0b6ee7ee3f24a
Chinmoy Das <chinmoy159@users.noreply.github.com>
2017-10-24 18:51:31 +0530

    user interface

---
+++ sample_2/ui.R
@@ -0,0 +1,14 @@
+library(shiny)
+shinyUI(fluidPage(
+        titlePanel ("Sample 3"),
+        sidebarLayout (
+                sidebarPanel (
+                        h5("This will create a slider with steps = 5"),
+                        sliderInput("slider", "Slide this", min = 1000, max = 2500, step = 5, value = 1000)
+                ),
+                mainPanel(
+                        p(h4("Here we'll output the value from the slider !"), hr(),"Value = "),
+                        textOutput("res"))
+                )
+        )
+)
